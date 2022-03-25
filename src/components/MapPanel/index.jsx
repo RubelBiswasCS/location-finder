64e66557c64493c2595a4f179e066c813d908ff9
RubelBiswasCS <rubelbiswas36@gmail.com>
2022-03-25 12:44:08 +0600

    added react-mapbox-map

--- src/components/MapPanel/index.jsx
+++ src/components/MapPanel/index.jsx
@@ -1,9 +1,33 @@
 import './index.scss';
+import 'mapbox-gl/dist/mapbox-gl.css';
 import * as React from 'react';
-
+import ReactMapGL from 'react-map-gl';
+import Container from '@mui/material/Container';
+// const mapboxAccessToken = 'pk.eyJ1IjoicnViZWxiaXN3YXMiLCJhIjoiY2wxNjBqbG1yMHVoODNkcWExZmc0Y2JvaiJ9.qshAlfWDxCblvr_MJkLg-Q'
+const mapboxAccessToken = process.env.REACT_APP_MAPBOX_TOKEN
+console.log(mapboxAccessToken)
 const MapPanel = () => {
+    const [viewport,setViewPort] = React.useState({
+        latitude:'23.8103',
+        longitude:'90.4125',
+        zoom:14,
+    })
+    // {
+    //     longitude: -122.4,
+    //     latitude: 37.8,
+    //     zoom: 14
+    // }
     return (
-        <>MapPanel</>
+        <Container className="map-panel">
+            <ReactMapGL
+            initialViewState={viewport}
+            mapboxAccessToken={mapboxAccessToken}
+            
+            mapStyle="mapbox://styles/mapbox/streets-v9"
+            >
+                markers here
+            </ReactMapGL>
+        </Container>
     );
 }
 export default MapPanel;
